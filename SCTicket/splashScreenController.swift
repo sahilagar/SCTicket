4edf3da8fef98157097f81e578f9e23da36c3900
Sahil Agarwal <sahilagarwal@Sahils-MacBook-Pro.local>
2017-08-03 16:58:22 -0700

    spash works

---
+++ SCTicket/splashScreenController.swift
@@ -0,0 +1,28 @@
+//
+//  splashScreenController.swift
+//  SCTicket
+//
+//  Created by Sahil Agarwal on 8/3/17.
+//  Copyright © 2017 Sahil Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class splashScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        perform(#selector(splashScreenController.showNavController), with: nil, afterDelay: 2)
+    }
+
+    func showNavController()
+    {
+        performSegue(withIdentifier: "splash", sender: self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+  
